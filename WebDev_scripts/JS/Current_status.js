c78b18e3ff990c3d0aa5ddd3afbf4bca1b7bb806
JDanielSecOps <josephdanielcharis@gmail.com>
2025-02-06 02:48:08 +0530

    Changes

--- WebDev_scripts/JS/Current_status.js
+++ WebDev_scripts/JS/Current_status.js
@@ -1,25 +1,80 @@
 
+import { format } from 'https://cdn.jsdelivr.net/npm/date-fns@2.28.0/esm/index.js';
 
 
-const heart_rate=document.getElementById("heart-rate")
-const leg_position=document.getElementById("leg-position")
-const head_position=document.getElementById("head-position")
 const date_time=document.getElementById("date-time")
-const seat_status=document.getElementById("Seat-status")
-const air_quality=document.getElementById("air-quality")
-const emergency=document.getElementById("emergency")
 
-function change_data(time,element,parameter){
+const Head_Status=document.getElementById("Head-Status")
+const Seat_Status=document.getElementById("Seat-Status")
+const Vitals_Status=document.getElementById("Vital-Status")
+const Leg_status =document.getElementById("Leg-status")
+const tilt_status=document.getElementById("Tilt-Status")
+
+const heart_rate_value=document.getElementById("Heart-rate")
+const Drowsiness_value=document.getElementById("Drowsiness")
+const Breathing_rate_value=document.getElementById("Breathing-rate")
+const Temperature_rate_value=document.getElementById("Temperature-rate")
+
+const hp1_value=document.getElementById("Head-1")
+const hp2_value=document.getElementById("Head-2")
+
+const leg1_value=document.getElementById("leg-1")
+const leg2_value=document.getElementById("leg-2")
+const leg3_value=document.getElementById("leg-3")
+const leg4_value=document.getElementById("leg-4")
+
+
+
+const tilt_value_x=document.getElementById("Tilt-x-values")
+const tilt_value_y=document.getElementById("Tilt-y-values")
+const tilt_value_z=document.getElementById("Tilt-z-values")
+
+const Alerts=document.getElementById("Alerts")
+
+function change_date(element,parameter){
     
     if(element.textContent !==parameter){
-        element.classList.add("text-transition")
-        element.textContent=parameter
+        let date=new Date(parameter)
+        let formatted_date=format(date,"MM/dd/yyyy HH:mm:ss")
+        element.textContent=formatted_date
     }
-    setTimeout(function(){
-        element.classList.remove("text-transition")
-    },time)
 }
 
+function change_status(element,parameter,Normal,Abnormal){
+    
+    if(element.textContent!==parameter){
+        if(parameter===0){
+            element.textContent=Normal
+            
+        }
+        else if(parameter===1){
+            element.textContent=Abnormal
+            
+        }
+    }
+}
+
+function change_value(element,parameter,unit){
+    if(element.textContent !==parameter){
+        element.textContent=parameter+` ${unit}`
+    }
+}
+
+function alerts(element,parameter){
+    if(element.textContent !==parameter){
+        if(parameter===0){
+            element.classList.remove("abnormal")
+            element.textContent="No Emergency"
+            element.classList.add("normal")
+            
+        }
+        else if(parameter===1){
+            element.classList.remove("normal")
+            element.textContent="Driver status check required kindly check the alerts tab"
+            element.classList.add("abnormal")
+        }
+    }
+}
 
 
 async function updater(time){
@@ -27,17 +82,37 @@ async function updater(time){
             fetch("/data")
             .then(response => response.json())
             .then(data =>{
-                change_data(1500,heart_rate,data.heartrate)
-                change_data(1500,date_time,data.date)
-                change_data(1500,head_position,data.head_position)
-                change_data(1500,seat_status,data.seat_status)
-                change_data(1500,leg_position,data.leg_position)
-                change_data(1500,emergency,data.emergency)
-                change_data(1500,air_quality,data.air_quality)
+                change_date(date_time,data.Date_and_time)
+
+                change_status(Seat_Status,data.Seat_Status,"Occupied","Unoccupied")
+                change_value(Drowsiness_value,data.Drowsiness_value," %")
+
+                
+                change_status(Vitals_Status,data.Vitals_Status,"Normal","Abnormal")
+                change_value(heart_rate_value,data.Heart_Rate,"BPM")
+                change_value(Breathing_rate_value,data.Breathing_Rate,"RR")
+                change_value(Temperature_rate_value,data.Temperature_Rate,"C")
+
+                change_status(Head_Status,data.Head_Status,"Normal","Abnormal")
+                change_value(hp1_value,data.Head_Position1,"mm")
+                change_value(hp2_value,data.Head_Position2,"mm")
+
+                change_status(Leg_status,data.Leg_Status,"Normal","Abnormal")
+                change_value(leg1_value,data.Leg_Status_1,"mm")
+                change_value(leg2_value,data.Leg_Status_2,"mm")
+                change_value(leg3_value,data.Leg_Status_3,"mm")
+                change_value(leg4_value,data.Leg_Status_4,"mm")
+
+                change_status(tilt_status,data.Tilt_status,"Normal","Abnormal")
+                change_value(tilt_value_x,data.Tilt_X,"degrees")
+                change_value(tilt_value_y,data.Tilt_Y,"degrees")
+                change_value(tilt_value_z,data.Tilt_Z,"degrees")
+                
+                alerts(Alerts,data.Alert_status)
                 
     })
     },time)}
 
 
-updater(2000)
+updater(1000)
 
---
+++ WebDev_scripts/JS/Drivers_Log_info.js
@@ -0,0 +1,15 @@
+const end_time=document.getElementById("To_time")
+const end_date=document.getElementById("end-date")
+const start_date=document.getElementById("start-date")
+const start_time=document.getElementById("From_time")
+const button =document.getElementById("button")
+const del_button =document.getElementById("button_del")
+
+button.addEventListener("click",function(){
+    
+    let start_time=start_time.value
+    let end_time=end_time.value
+
+    
+
+})
